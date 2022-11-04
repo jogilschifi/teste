539657d06682f3d30275c91eab7d9d153c7ddd17
JoaoGilberto <joao_schinemann@hotmail.com>
2022-11-04 20:48:25 -0300

    make it better

--- app/views.py
+++ app/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from .forms import UserRegisterForm
 
 import datetime
 
@@ -61,7 +62,7 @@ class CustomLoginView(LoginView):
 
 class RegisterPage(FormView):
     template_name = 'app/register.html'
-    form_class = UserCreationForm
+    form_class = UserRegisterForm
     redirect_authenticated_user = True
     success_url = reverse_lazy('home')
 
