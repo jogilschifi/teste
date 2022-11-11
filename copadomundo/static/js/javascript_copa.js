a17724a914822dd7d96748d9c1be6240324d75a3
JoaoGilberto <joao_schinemann@hotmail.com>
2022-11-10 22:09:55 -0300

    make it better

--- copadomundo/static/js/javascript_copa.js
+++ copadomundo/static/js/javascript_copa.js
@@ -2,3 +2,36 @@
     "use strict";
 
 })(window,document);
+
+  var visibleDiv = 0;
+  function showDiv()
+  {
+    $(".grid").hide();
+    $(".grid:eq("+ visibleDiv +")").show();
+  }
+  showDiv()
+
+function showNext()
+{
+  if(visibleDiv== $(".grid").length-1)
+  {
+    visibleDiv = 0;
+  }
+  else {
+    visibleDiv ++;
+  }
+  showDiv();
+}
+
+
+function showPrev()
+{
+  if (visibleDiv == 0)
+  {
+    visibleDiv= $(".grid").length-1
+  }
+  else {
+    visibleDiv --;
+  }
+  showDiv();
+}
